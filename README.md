f41967b851bf6499c8cc8add63dfd6fde5427e3c
kuraian <ryan.m.diep@gmail.com>
2024-05-02 21:28:26 -0700

    completely refactored into a module
    removed selenium dependency (overkill), wrote as class module for reusability, changed formatting and docstrings

--- README.md
+++ README.md
@@ -1,43 +1,46 @@
 # tcgplayer scraper
 
-tcgp_scraper.py is a script implementing undetected_chromedriver and sends POST requests using selenium-requests to scrape financial data off tcgplayer
+tcgp_scraper.py is a module implementing httpx for scraping financial data off tcgplayer
 
 ## install dependencies:
 
-it uses undetected-chromedriver and seleniumrequests, so installation goes:
+it uses httpx so:
+
 ```default
-pip install undetected-chromedriver
-```
-```default
-pip install selenium-requests
+pip install httpx
 ```
 
 ### how to use:
-* either run the script as is (change the id variable for whatever set id you want) or import the functions into your own script
-* the data will be stored in ./sets/{set_id}
+
+- either run the script as is (change the id variable for whatever set id you want) or import the functions into your own script
+- the data will be stored in ./sets/{set_id}
 
 example
+
 ```python
 import tcgp_scraper as tcgp
 
 
-# set IDs can be found here: https://mpapi.tcgplayer.com/v2/Catalog/SetNames
-set_id = 1
+async def main():
+    set_id = '23228'
+    scraper = tcgp.TCGPScraper()
+    await scraper.load_set(set_id)
+    cards = scraper.get_card_ids()
+    sales = await scraper.get_card_sales(cards[0], end=2)  # For example's sake, gets the 50 most recent sales of a single card.
+    print(sales)
 
-cards = tcgp.get_cards(set_id)
-card_ids = get_card_ids(set_id, cards)
 
-for card_id in card_ids:
-  get_card_sales(set_id, card_id)
+if __name__ == '__main__':
+    asyncio.run(main())
 ```
 
 ## todo:
-* implement threading + proxies, or make it deployable, or both
-* build a flask frontend?
-* change order of parameters in functions (lazy)
-* change directory building, or allow user to set it by parameter (mega lazy)
+
+- implement threading + proxies
+- build and deploy using docker + k8s
+- build a flask app
+- r/w to mongodb
 
 ## issues:
-* not conventionally deployable using selenium-grid (CRITICAL)
-* not currently detected, but needs to be run through proxies or distributed (see above)
-* expensive (selenium.....)
+
+- not currently detected, but should be run through proxies (see above)
--- tcgp_scraper.py
+++ tcgp_scraper.py
@@ -1,139 +1,168 @@
-import undetected_chromedriver as uc
-from seleniumrequests.request import RequestsSessionMixin
-import requests
-import json
-import random
+"""A module including the class TCGPScraper, used for scraping TCGPlayer.com
+
+Typical usage example:
+
+    set_id = '23228'
+    scraper = TCGPScraper()
+    scraper.load_set(set_id)
+"""
+
+import asyncio
+import httpx
+import sys
 import time
-import os
+import re
 
 
-class MyCustomWebDriver(RequestsSessionMixin, uc.Chrome):
-    pass
+class TCGPScraper:
+    """A scraper for TCGPlayer.com sales information.
 
-
-def payload_generator(n):
-    """Generates a payload for a given offset
-    Args:
-        n: the current offset, starts at 1
-    Returns:
-        A JSON payload for a POST request to
-            https://mpapi.tcgplayer.com/v2/product/{id}/latestsales
+    Attributes:
+        No public attributes
     """
 
-    payload = {
-        "listingType": "All",
-        "offset": n*25,
-        "limit": 25,
-        "time": round(time.time() * 1000)
-    }
-    return payload
+    def __init__(self, set_id: str = None) -> None:
+        self._set_id = None
+        self._cards = None
+        self._card_ids = None
+        if set_id:
+            self.load_set(set_id)
 
+    async def __payload_generator(self, n: int) -> dict:
+        """Generates a payload for a given offset.
 
-def get_cards(set_id):
-    """Gets a JSON of card entries from tcgplayer for a set_id, writes them to a file,
-        and returns a dict for those cards
-    Args:
-        id: a set's id number
-    Returns:
-        returns a dict of cards
-    todo:
-        implement proxies or distribution
-    """
+        Args:
+            n: An integer for the current offset.
 
-    response = requests.get(
-        f"https://infinite-api.tcgplayer.com/priceguide/set/{set_id}/cards/?rows=5000",)
+        Returns:
+            A JSON payload for a POST request to
+                https://mpapi.tcgplayer.com/v2/product/{id}/latestsales
+        """
+        payload = {
+            "listingType": "All",
+            "offset": n * 25,
+            "limit": 25,
+            "time": round(time.time() * 1000),
+        }
+        return payload
 
-    path = f"sets/{set_id}"
-    if not os.path.isdir(path):
-        os.mkdir(path)
-    with open(path + "/cards.json", "w") as f:
-        cards = response.json()
-        f.write(json.dumps(cards, indent=4))
-    return cards
-
-
-def get_card_ids(set_id, cards):
-    """extracts the card_ids from a collection of cards
-    Args:
-        set_id:
-        cards: dict of cards
-    Returns:
-        list of unique card_ids
-    """
-
-    card_ids = set()
-    for card in cards["result"]:
-        card_ids.add(card["productID"])
-    card_ids = list(card_ids)
-    path = f"sets/{set_id}"
-
-    if not os.path.isdir(path):
-        os.mkdir(path)
-    with open(path + "/card_ids.json", "w") as f:
-        f.write(json.dumps({"card_ids": card_ids}, indent=4))
-        f.close()
-    return card_ids
-
-
-def get_card_sales(set_id, card_id):
-    """Gets all card sales for a given card id
-    Args:
-        set_id
-        card_id
-    Returns:
-        A list of sales
-    """
-
-    offset = 1
-    next_page = True
-    sales = []
-    api_url = f"https://mpapi.tcgplayer.com/v2/product/{card_id}/latestsales?mpfev=2372"
-
-    try:
-        custom_webdriver = MyCustomWebDriver()
-        while next_page:
-            response = custom_webdriver.request(
-                "POST", api_url, json=payload_generator(offset))
-            data = response.json()
-            sales.extend(data["data"])
-            if not data["nextPage"] == "Yes":
-                next_page = False
-            else:
-                # time.sleep(random.triangular(1.2, 2.3))
-                time.sleep(.1)
-                offset += 1
-        path = f"sets/{set_id}"
-        if not os.path.isdir(path):
-            os.mkdir(path)
-        with open(f"{path}/{card_id}.json", "w") as f:
-            f.write(json.dumps({"sales": sales}, indent=4))
-            f.close()
-    except Exception as e:
-        print(f"Error for card {card_id}.")
-
-    custom_webdriver.close()
-    time.sleep(1)
-
-    return sales
-
-
-def main():
-    # its easier to just lookup the set ID manually than automate it
-    # https://mpapi.tcgplayer.com/v2/Catalog/SetNames
-
-    # obsidian flames
-    set_id = 23228
-
-    cards = get_cards(set_id)
-    card_ids = get_card_ids(set_id, cards)
-    card_ids.sort()
-    for card_id in card_ids:
-        if not os.path.isfile(f"sets/{set_id}/{card_id}.json"):
-            print(f"{card_id} does not exist!!!")
-            get_card_sales(set_id, card_id)
+    async def __request_cards(self) -> None:
+        """GETs and stores JSON of card entries from TCGPlayer for self._set_id into self._cards.
+        """  # noqa: E501
+        try:
+            response = httpx.get(
+                'https://infinite-api.tcgplayer.com/priceguide/'
+                f'set/{self._set_id}/cards/?rows=5000',
+            )
+            response.raise_for_status()
+        except httpx.HTTPError as exc:
+            print(
+                f'HTTP Exception for {exc.request.url} - {exc}',
+                file=sys.stderr,
+            )
+            sys.exit(1)
         else:
-            print(f"{card_id} does exist!!!")
+            self._cards = response.json()
+
+    async def __extract_card_ids(self) -> None:
+        """Extracts the card_ids from _cards."""
+        unique_ids = set()
+        try:
+            for card in self._cards['result']:
+                unique_ids.add(str(card['productID']))
+        except Exception as exc:
+            print(
+                f'Unexpected error in __extract_card_ids - {exc}',
+                file=sys.stderr,
+            )
+            sys.exit(1)
+        else:
+            self._card_ids = list(unique_ids)
+
+    async def load_set(self, set_id: str) -> None:
+        """Loads a set_id and requests and extracts cards from that set."""
+        self._set_id = set_id
+        await self.__request_cards()
+        await self.__extract_card_ids()
+
+    async def get_card_sales(self, card_id: str, end: int = None) -> list:
+        """Gets all card sales for a given card id and ending offset
+        Args:
+            card_id: A card_id string.
+
+        Returns:
+            A list of sales JSON. For example:
+
+            [
+                {
+                    'condition': 'Near Mint',
+                    'variant': 'Reverse Holofoil',
+                    'language': 'English',
+                    'quantity': 1,
+                    'title': 'Altaria',
+                    'listingType': 'ListingWithoutPhotos',
+                    'customListingId': '0',
+                    'purchasePrice': 0.24,
+                    'shippingPrice': 0.0,
+                    'orderDate': '2024-04-24T01:53:25.003+00:00'
+                },
+            ]
+
+        Raises:
+            ValueError: A card_id with invalid format was passed as an argument.
+        """  # noqa: E501
+        # Check for a valid card_id
+        if not re.match(r'^\d{6}$', card_id):
+            raise ValueError(
+                'A card_id with invalid format was passed '
+                f'as an argument - {card_id}'
+            )
+
+        offset = 1
+        next_page = True
+        sales = []
+        api_url = (
+            f'https://mpapi.tcgplayer.com/v2/product/{card_id}/latestsales?mpfev=2399'  # noqa: E501
+        )
+
+        async with httpx.AsyncClient() as client:
+            while next_page:
+                payload = await self.__payload_generator(offset)
+                try:
+                    response = await client.post(api_url, json=payload)
+                    response.raise_for_status()
+                except httpx.HTTPError as exc:
+                    print(
+                        f'HTTP Exception for {exc.request.url} - {exc}',
+                        file=sys.stderr,
+                    )
+                else:
+                    data = response.json()
+                    sales.extend(data['data'])
+                    if data['nextPage'] == 'Yes' and offset != end:
+                        time.sleep(0.1)
+                        offset += 1
+                    else:
+                        next_page = False
+        return sales
+
+    def get_cards(self) -> dict:
+        """Getter for self._cards."""
+        return self._cards
+
+    def get_card_ids(self) -> dict:
+        """Getter for self._card_ids."""
+        return self._card_ids
 
 
-if __name__ == "__main__":
-    main()
+async def main():
+    set_id = '23228'
+    scraper = TCGPScraper()
+    await scraper.load_set(set_id)
+    cards = scraper.get_card_ids()
+    sales = await scraper.get_card_sales(cards[0], end=2)
+    print(sales)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
